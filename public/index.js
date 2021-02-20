a13845164098b6a36192d357112d1c136d57a9f7
Doug <hathemagicpenguin@gmail.com>
2021-02-19 23:59:20 -0500

    Button (#3)
    Overview
    Created working buttons, and began work on an array/for loop system to start and end each section, in terms of visibility.
    
    HTML/CSS
    index.css
    Added Data-Type selector and visibility attributes.
    index.html
    Rearranged and added class bttn to sections, as to help forward the buttons.
    Rearranged the jQuery and Bootstrap CDNs.
    
    Rearranged the jQuery and Bootstrap CDNs in highscores.html
    
    Javascript
    index.js
    Continued jQuery conversion.
    Created an array of selectors with which to loop through.
    Set some delay so the sections do not immediately disappear, for later warnings/notifications.
    Verified that, for the time being, the first section is clickable and makes the next section visible the current one hidden.
    
    Repository
    n/a

--- public/index.js
+++ public/index.js
@@ -1,16 +1,36 @@
-var container = $(".container");
+const container = $(".bttn");
 
-container.addEventListener("click", function(event) {
-  var element = event.target;
+const buttonArray = [
+  $("#begin"), $("#question1"), $("#question2"), $("#question3"), $("#question4"), $("#question5"),
+  $("#question6"), $("#question7"), $("#question8"), $("#question9"), $("#question10"), $("#end")
+]
 
-  if (element.matches("div")) {
-    var state = element.getAttribute("data-state");
-    if (state === "hidden") {
-      element.dataset.state = "visible";
-      element.setAttribute("data-state", "visible");
-    } else {
-      element.dataset.state = "hidden";
-      element.innerHTML = null;
+container.on("click", function (event) {
+  // console.log("Clicks");
+  const element = $(event.target);
+  console.log(buttonArray[0]);
+  for (let i = 1; i < buttonArray.length; i++) {
+    if (element.is("section")) {
+      // console.log("Section");
+      const state = element.attr("data-state");
+      if (state === "hidden") {
+        setTimeout(function () {
+          // console.log("hidden");
+          element.data("visible", "visible");
+          element.css("visibility", "visible");
+        },
+          5000);
+      } else {
+        setTimeout(function () {
+          // console.log("visible");
+          element.css("visibility", "hidden");
+          element.data("visible", "hidden");
+          buttonArray[i].css("visibility", "visible");
+          buttonArray[i].data("visible", "visible");
+        },
+          5000);
+      }
     }
+    
   }
 });
