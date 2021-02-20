9e41206da8075f83674cde5d3134ab0bd959eb1d
Doug <hathemagicpenguin@gmail.com>
2021-02-20 08:40:55 -0500

    Button (#4)
    Overview
    Button now displays only next button section, not all.
    
    HTML/CSS
    n/a
    
    Javascript
    index.js
    Made an if statement to check that element is equal to an element on the array.
    Made the next element of the array visible.
    
    Repository
    n/a

--- public/index.js
+++ public/index.js
@@ -8,28 +8,36 @@ const buttonArray = [
 container.on("click", function (event) {
   // console.log("Clicks");
   const element = $(event.target);
-  console.log(buttonArray[0]);
-  for (let i = 1; i < buttonArray.length; i++) {
-    if (element.is("section")) {
-      // console.log("Section");
-      const state = element.attr("data-state");
-      if (state === "hidden") {
-        setTimeout(function () {
-          // console.log("hidden");
-          element.data("visible", "visible");
-          element.css("visibility", "visible");
-        },
-          5000);
-      } else {
-        setTimeout(function () {
-          // console.log("visible");
-          element.css("visibility", "hidden");
-          element.data("visible", "hidden");
-          buttonArray[i].css("visibility", "visible");
-          buttonArray[i].data("visible", "visible");
-        },
-          5000);
-      }
+  for (let i = 0; i < buttonArray.length; i++) {
+    console.log("still running");
+    if (element.is(buttonArray[i])) {
+      // console.log("Match");
+      setTimeout(function () {
+        buttonArray[i]
+        buttonArray[i+1].css("visibility", "visible");
+        buttonArray[i+1].data("visible", "visible");
+      },
+        5000);
+      if (element.is("section")) {
+          // console.log("Section");
+          const state = element.attr("data-state");
+          if (state === "hidden") {
+            setTimeout(function () {
+              // console.log("hidden");
+              element.data("visible", "visible");
+              element.css("visibility", "visible");
+            },
+              5000);
+          } else {
+            setTimeout(function () {
+              // console.log("visible");
+              element.css("visibility", "hidden");
+              element.data("visible", "hidden");
+            },
+              5000);
+          }
+        }
+      break;
     }
     
   }
