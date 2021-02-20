f4b26cfb1b70d9277f9c48f949e178ccbca4803f
Doug <hathemagicpenguin@gmail.com>
2021-02-20 15:01:08 -0500

    Quiz/Button/Score/Restructure (#5)
    Overview
    Added more specifics to the css page.
    Added Begin button features.
    Bound container click to single out the button and it's parent.
    Set a working countdown.
    Made interacting buttons for main page and highscore page.
    Condensed highscores page into index page for simplification of JS.
    Added Wrong and Right sections.
    Added actual quiz questions.
    
    HTML/CSS
    index.html
    Changed around class names for most everything to better match the naming conventions.
    Set timer to start at 60.
    Created 5 questions for the quiz.
    Created Wrong and Right sections to show whether your answer was wrong or right.
    Created a section for the Highscores page.
    index.css
    Added three new selectors and attributes to better keep display values in control.
    
    Javascript
    index.js
    ~ Added numerous new variables.
    ~ Shortened quiz array.
    ~ Established scores
    ~ Established Begin button to start the quiz, which starts the quiz, the timer, and constantly checks for the end section
    ~ Fixed container button click event to better smooth showing and hiding sections, and show the correct and incorrect sections.
    ~ Created a standalone Interval to detect when the End section is shown.
    ~ Created a submit button event that launches the end of the quiz, and resets it for the next user.
    ~ Created click events for both pages.
    
    
    Repository
    Deleted highscores.html

--- public/index.js
+++ public/index.js
@@ -1,44 +1,134 @@
-const container = $(".bttn");
+const begin = $("#begin");
+const container = $(".section");
+const end = $("#end")
+const finalScore = $(".highscore");
+const initials = $("#initials");
+const scoreboard = $("#scoreboard")
+const submission = $("#submission");
+const submitArray = [];
+const time = $(".time");
+const hpLink = $(".hpLink");
+const qpLink = $(".qpLink");
+const qp = $(".quizPage");
+const hp = $(".highscoresPage");
 
 const buttonArray = [
-  $("#begin"), $("#question1"), $("#question2"), $("#question3"), $("#question4"), $("#question5"),
-  $("#question6"), $("#question7"), $("#question8"), $("#question9"), $("#question10"), $("#end")
+  begin, $("#question1"), $("#question2"), $("#question3"), $("#question4"), $("#question5"),
+  end
 ]
+
+let score = 0;
+let highscore = 0;
+
+begin.on("click", function (event) {
+  // console.log("Begin click");
+  let timer = 60;
+  const beginning = setInterval(() => {
+    const endTime = end.data("visible");
+    // console.log("Running");
+    if (endTime === "visible") {
+      // console.log("done");
+      clearInterval(beginning);
+    }
+    timer--;
+    if (timer > 0) {
+      time.text(timer);
+    } else if (timer === 0) {
+      clearInterval(beginning);
+      time.text(0);
+      for (let i = 1; i < 6; i++) {
+        buttonArray[i].data("visible", "hidden");
+        buttonArray[i].css("display", "none");
+        end.data("visible", "visible");
+        end.css("display", "inline");
+      }
+      return;
+    }
+  }, 1000);
+});
 
 container.on("click", function (event) {
   // console.log("Clicks");
-  const element = $(event.target);
+  const element = $(event.target).parent();
+  const button = $(event.target);
+  const win = $(".win");
+  const lose = $(".lose");
   for (let i = 0; i < buttonArray.length; i++) {
-    console.log("still running");
-    if (element.is(buttonArray[i])) {
-      // console.log("Match");
-      setTimeout(function () {
-        buttonArray[i]
-        buttonArray[i+1].css("visibility", "visible");
-        buttonArray[i+1].data("visible", "visible");
-      },
-        5000);
-      if (element.is("section")) {
-          // console.log("Section");
-          const state = element.attr("data-state");
-          if (state === "hidden") {
-            setTimeout(function () {
-              // console.log("hidden");
-              element.data("visible", "visible");
-              element.css("visibility", "visible");
-            },
-              5000);
-          } else {
-            setTimeout(function () {
-              // console.log("visible");
-              element.css("visibility", "hidden");
-              element.data("visible", "hidden");
-            },
-              5000);
-          }
-        }
-      break;
+    // console.log("still running");
+    if (element.is(undefined)) {
+       return;
+     } else if ((element.is(buttonArray[i])) && (button.is("button"))) {
+       // console.log("Match");
+       buttonArray[i + 1].data("visible", "visible");
+       buttonArray[i + 1].css("display", "inline");
+       element.data("visible", "hidden");
+       element.css("display", "none");
     }
-    
+  }
+  if (button.is(".correct")) {
+    // console.log("correct");
+    win.css("display", "inline");
+    setTimeout(function () {
+      win.css("display", "none");
+    },
+      1000
+    );
+    return score++;
+  } else if (button.is(".incorrect")) {
+    // console.log("incorrect");
+    lose.css("display", "inline");
+    setTimeout(function () {
+      lose.css("display", "none");
+    },
+      1000
+    );
+  }
+});
+
+const findEnd = setInterval(() => {
+  const endTime = end.data("visible");
+  const countdown = time.text();
+  if (endTime === "visible") {
+    // console.log("done");
+    clearInterval(findEnd);
+    const finish = countdown * score;
+    let highscore = finish;
+    finalScore.text(highscore);
+    return highscore;
+  }
+}, 1000);
+
+submission.on("click", () => {
+  const inputText = initials.val();
+  const inputNumber = finalScore.text();
+  submitArray.push(inputText + ": " + inputNumber);
+  scoreboard.append("<li>" + submitArray[0] + "</li>");
+  submitArray.pop();
+  initials.val("");
+  end.data("visible", "hidden");
+  end.css("display", "none");
+  begin.data("visible", "visible");
+  begin.css("display", "inline");
+  qp.css("display", "none");
+  hp.css("display", "inline");
+  let score = 0;
+  let highscore = 0;
+  time.text(60);
+  return;
+});
+
+qpLink.on("click", () => {
+  if (hp.css("display", "inline")) {
+    // console.log("HP Shown");
+    hp.css("display", "none");
+    qp.css("display", "inline");
+  }
+});
+
+hpLink.on("click", () => {
+  if (qp.css("display", "inline")) {
+    // console.log("QP Shown");
+    qp.css("display", "none");
+    hp.css("display", "inline");
   }
 });
